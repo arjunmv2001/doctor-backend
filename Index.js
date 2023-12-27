996e7589e0e6fca6edef8bae7f3d52e536f0cb87
arjunmv2001 <arjunmv6677@gmail.com>
2023-12-27 14:48:10 +0530

    Backend for doctor-app

--- Index.js
+++ Index.js
@@ -1,17 +1,23 @@
 // import json server
 const jsonServer = require('json-server')
+
 //create a server
 const Server = jsonServer.create()
+
 //setup path for db.json file
-const router = jsonServer.router("db.json")
+const router = jsonServer.router('db.json')
+
 //Return Middleware used by json server
 const Middleware = jsonServer.defaults()
+
 //setup port for server
-const port = process.env.port||3001
+const port = process.env.port || 3001
+
 //Use middleware and server
 Server.use(Middleware)
 Server.use(router)
+
 //To run the server
-Server.listen(port,()=>{
+Server.listen(port, () => {
     console.log('Listening on port ' + port);
 })
